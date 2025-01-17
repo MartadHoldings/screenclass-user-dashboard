240e22427b83e04abe11de86db3962a304eb1a54
timmie20 <olutimilehinolayinka1234@gmail.com>
2025-01-17 13:23:46 +0100

    feat:timi:(Add new folder and route path structurs)

---
+++ src/app/student/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Overview } from "@/features/student/overview";
+
+export default function page() {
+  return (
+    <div>
+      <Overview />
+    </div>
+  );
+}
---
+++ src/features/guardian/settings/settings.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Settings = () => {
+  return <div>this is the settings page for the guardian dashboard </div>;
+};
